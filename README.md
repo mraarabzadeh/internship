2434bf31b11680a35f4ef329c59ce686215e092d
mra <mra_arabzadeh@yahoo.com>
2019-09-04 16:07:39 +0430

    use hazm tokenizer and write readme file

---
+++ README.md
@@ -0,0 +1,10 @@
+for using this app you can get input from shel or it can get it from file if input bind to app in shel. for get input from file, the last paragraph of file must end with $ charecter.
+
+i found best coeficient and put them in result2. must of them are good but the last items normaly behave better than others and because of this for run i use the last coeficient
+
+for get result now it read coeficiency from last line of result2 file. if you want to change it you must change parameter of set_mul() function.
+
+it has ability for searching to find best coeficiency by iterating them in search_for_best_mul() function. if expexted parameter send to it it check result of execution with expexted result and with don't get this parameter it only run check and show result of run.
+
+NOTICE!!!
+this app has very long time for initializing ctcclass but after that it  has good speed.
--- find_mul.py
+++ find_mul.py
@@ -1,6 +1,7 @@
 import torch
 from ctcdecode import *
 import sys, os, ctcdecode 
+from hazm import Normalizer,sent_tokenize
 
 
 
@@ -23,15 +24,15 @@ def set_mul(i,j,k,space_indices, sentence_for_beam,ctc_prob):
 		if sentence_for_beam[x] == ' ':
 			ctc_prob[x][SPACE_LOCATION] = .5
 			ctc_prob[x][SEMI_SPACE_LOCATION] = .3
-			# ctc_prob[x][-2] = .1
+			ctc_prob[x][-2] = .1
 		elif sentence_for_beam[x] == '‌':
 			ctc_prob[x][SPACE_LOCATION] = .05
 			ctc_prob[x][SEMI_SPACE_LOCATION] = .5
-			# ctc_prob[x][-2] = .1
+			ctc_prob[x][-2] = .1
 		else:
 			ctc_prob[x][SPACE_LOCATION] = i
 			ctc_prob[x][SEMI_SPACE_LOCATION] = j
-			# ctc_prob[x][-2] = k
+			ctc_prob[x][-2] = k
 	ctc_prob[-1][-1] = 1
 
 
@@ -84,10 +85,7 @@ def prepare_line_for_search(line, is_concrete=False):
 		sentence = line
 	sentence_for_beam = ''
 	for x in range(len(sentence)):
-		if x == len(sentence)-1:
-			sentence_for_beam += sentence[x]
-			continue
-		elif sentence[x] != ' ' and sentence[x+1] != ' ' and x != len(sentence)-1:
+		if sentence[x] != ' ' and x != len(sentence)-1 and sentence[x+1] != ' ' :
 			sentence_for_beam += sentence[x] + '_'
 			space_indices.append(len(sentence_for_beam) - 1)
 		else:
@@ -101,6 +99,11 @@ def make_ctc_matrix(sentence_for_beam):
 	return [[1 if alpha[y] == x else 0 for y in range(len(alpha))]for x in sentence_for_beam]
 
 
+def tokenize(paragraph, wanted_list):
+	normal = Normalizer(remove_extra_spaces=True, punctuation_spacing=True, persian_style=False,persian_numbers=False, remove_diacritics=False, affix_spacing=False, token_based=False)
+	for sentence in sent_tokenize(normal.normalize(paragraph)):
+		wanted_list.append(sentence)
+
 def read_multi_paragraph_text():
 	hole_text = []
 	while True:
@@ -108,9 +111,9 @@ def read_multi_paragraph_text():
 		if len(paragraph) == 0:
 			continue
 		if paragraph[-1] == '$':
-			hole_text.append(paragraph[:-1])
+			tokenize(paragraph[:-1],hole_text)
 			break
-		hole_text.append(paragraph)
+		tokenize(paragraph,hole_text)
 	return hole_text
 
 
@@ -127,12 +130,13 @@ decoder = ctcdecode.CTCBeamDecoder(alpha, beam_width=20,
 			                                   blank_id=alpha.index('_'),
 			                                   model_path=lm_path, alpha=.45, beta=3)
 
-
+#for get input from shell
+#line = sys.argv[1:]
+#prepare_line_for_search(line)
 hole_text = read_multi_paragraph_text()
 for paragraph in hole_text:
 	sentence_for_beam, space_indices = prepare_line_for_search(paragraph,True)
 	ctc_prob = make_ctc_matrix(sentence_for_beam)
 	item = read_from_file('result2')[-1]
 	set_mul(item[0], item[1], item[2], space_indices, sentence_for_beam, ctc_prob)
-	if (run_ctcdecoder(decoder,ctc_prob)):
-		print(item[0], item[1], item[2])
+	run_ctcdecoder(decoder,ctc_prob)
